69171efc70c3e04dc3dfd016679f69b155f25e30
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-06-11 13:49:53 +0200

    Change js script into one file: add more functions

--- web/js/snippets/FooterSnippet.js
+++
@@ -1,3 +0,0 @@
-document.write("<footer>\n" +
-    "        <p>Chlur&Chlurs Company &copy; 2019</p>\n" +
-    "    </footer>")
--- web/js/snippets/HeaderSnippet.js
+++
@@ -1,6 +0,0 @@
-document.write("\
-<header>\
-    <p>Logged as: name(JS)</p>\
-    <button type=\"button\">Logout</button>\
-    <h1>Quest Store</h1>\
-</header>")
