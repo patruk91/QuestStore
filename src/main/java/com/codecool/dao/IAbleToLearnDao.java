3ac1cf277daf03895c4d23da66ba797d21657658
Krzysiek <krzysiekczaja@gmail.com>
2019-07-09 11:10:54 +0200

    Add methods to IStudentDao, IAbleToLearnDao and empty implementation to StudentSQL class

--- src/main/java/com/codecool/dao/IAbleToLearnDao.java
+++ src/main/java/com/codecool/dao/IAbleToLearnDao.java
@@ -1,4 +1,8 @@
 package com.codecool.dao;
 
+import com.codecool.model.Student;
+
 public interface IAbleToLearnDao {
+    public String getClassName(Student student);
+    public void addStudentToClass(Student student);
 }
--- src/main/java/com/codecool/dao/sql/StudentSQL.java
+++ src/main/java/com/codecool/dao/sql/StudentSQL.java
@@ -2,6 +2,44 @@ package com.codecool.dao.sql;
 
 import com.codecool.dao.IAbleToLearnDao;
 import com.codecool.dao.IStudentDao;
+import com.codecool.model.Mentor;
+import com.codecool.model.Student;
+
+import java.util.List;
 
 public class StudentSQL implements IStudentDao, IAbleToLearnDao {
+    @Override
+    public String getClassName(Student student) {
+        return null;
+    }
+
+    @Override
+    public void addStudentToClass(Student student) {
+
+    }
+
+    @Override
+    public void addStudent(Student student) {
+
+    }
+
+    @Override
+    public void updateStudent(Student student) {
+
+    }
+
+    @Override
+    public void deleteStudent(Student student) {
+
+    }
+
+    @Override
+    public List<Student> getAllStudents(Mentor mentor) {
+        return null;
+    }
+
+    @Override
+    public Student getStudent(int id) {
+        return null;
+    }
 }
