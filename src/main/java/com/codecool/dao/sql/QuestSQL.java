8fe5ce2a870fbde8fdd311bf241c0a5283ca43f8
Krzysiek <krzysiekczaja@gmail.com>
2019-07-09 11:18:30 +0200

    Add methods to IQuestDao and empty implementation in QuestSQL class

--- src/main/java/com/codecool/dao/sql/QuestSQL.java
+++ src/main/java/com/codecool/dao/sql/QuestSQL.java
@@ -1,6 +1,39 @@
 package com.codecool.dao.sql;
 
 import com.codecool.dao.IQuestDao;
+import com.codecool.model.Quest;
+import com.codecool.model.Student;
+
+import java.util.List;
 
 public class QuestSQL implements IQuestDao {
+    @Override
+    public void addQuest(Quest quest) {
+
+    }
+
+    @Override
+    public void updateQuest(Quest quest) {
+
+    }
+
+    @Override
+    public void deleteQuest(Quest quest) {
+
+    }
+
+    @Override
+    public List<Quest> getAllQuests() {
+        return null;
+    }
+
+    @Override
+    public Quest getQuest(int id) {
+        return null;
+    }
+
+    @Override
+    public void asignQuest(Student student, int questId) {
+
+    }
 }
