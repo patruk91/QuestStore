6acef0597665c512af643c778401cfaf9a006073
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-07-09 14:07:24 +0200

    Implement recurring generic patter for admin class

--- src/main/java/com/codecool/model/Admin.java
+++ src/main/java/com/codecool/model/Admin.java
@@ -1,4 +1,21 @@
 package com.codecool.model;
 
 public class Admin extends User {
+    protected Admin(UserBuilder<?> builder) {
+        super(builder);
+    }
+
+    public static class AdminBuilder extends User.UserBuilder<AdminBuilder> {
+        private Admin adminToBuild;
+
+        public AdminBuilder() {
+            this.adminToBuild = new Admin(this);
+        }
+
+        public Admin build() {
+            Admin buildUser = this.adminToBuild;
+            this.adminToBuild = new Admin(this);
+            return buildUser;
+        }
+    }
 }
