a305c2f15c229929b86d6fbade2e3e96e7f97586
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-07-09 13:06:09 +0200

    Implement methods for StudentProfile Class: add getters and setters

--- src/main/java/com/codecool/model/StudentProfile.java
+++ src/main/java/com/codecool/model/StudentProfile.java
@@ -1,4 +1,31 @@
 package com.codecool.model;
 
 public class StudentProfile {
+    private int coins;
+    private int experience;
+    private int classId;
+
+    public int getCoins() {
+        return coins;
+    }
+
+    public void setCoins(int coins) {
+        this.coins = coins;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public int getClassId() {
+        return classId;
+    }
+
+    public void setClassId(int classId) {
+        this.classId = classId;
+    }
 }
