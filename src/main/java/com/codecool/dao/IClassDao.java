ec1ac3b4837715ce4e8aeaa50ee2cb375156ac96
Krzysiek <krzysiekczaja@gmail.com>
2019-07-09 11:30:48 +0200

    Add methods to IClassDao and empty implementations in ClassSQL class

--- src/main/java/com/codecool/dao/IClassDao.java
+++ src/main/java/com/codecool/dao/IClassDao.java
@@ -1,4 +1,13 @@
 package com.codecool.dao;
 
+import com.codecool.model.ClassGroup;
+
+import java.util.List;
+
 public interface IClassDao {
+    public void addClass(ClassGroup classGroup);
+    public void updateClass(ClassGroup classGroup);
+    public void removeClass(ClassGroup classGroup);
+    public List<ClassGroup> getAllClasses();
+    public ClassGroup getClass(int classId);
 }
--- src/main/java/com/codecool/dao/sql/ClassSQL.java
+++ src/main/java/com/codecool/dao/sql/ClassSQL.java
@@ -1,6 +1,33 @@
 package com.codecool.dao.sql;
 
 import com.codecool.dao.IClassDao;
+import com.codecool.model.ClassGroup;
+
+import java.util.List;
 
 public class ClassSQL implements IClassDao {
+    @Override
+    public void addClass(ClassGroup classGroup) {
+        
+    }
+
+    @Override
+    public void updateClass(ClassGroup classGroup) {
+
+    }
+
+    @Override
+    public void removeClass(ClassGroup classGroup) {
+
+    }
+
+    @Override
+    public List<ClassGroup> getAllClasses() {
+        return null;
+    }
+
+    @Override
+    public ClassGroup getClass(int classId) {
+        return null;
+    }
 }
