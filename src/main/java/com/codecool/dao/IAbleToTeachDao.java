883025ca48635a99685abd62485ce5c4a77cbe3f
Krzysiek <krzysiekczaja@gmail.com>
2019-07-09 10:54:28 +0200

    Add mentods to IAdminDao and IAbleToTeachDao with empty implementation in MentorSQL class

--- src/main/java/com/codecool/dao/IAbleToTeachDao.java
+++ src/main/java/com/codecool/dao/IAbleToTeachDao.java
@@ -1,4 +1,8 @@
 package com.codecool.dao;
 
+import com.codecool.model.Mentor;
+
 public interface IAbleToTeachDao {
+    public void addMentorToClass(Mentor mentor);
+    public void getAllStudentsFromClass(Mentor mentor);
 }
--- src/main/java/com/codecool/dao/sql/MentorSQL.java
+++ src/main/java/com/codecool/dao/sql/MentorSQL.java
@@ -2,6 +2,43 @@ package com.codecool.dao.sql;
 
 import com.codecool.dao.IAbleToTeachDao;
 import com.codecool.dao.IMentorDao;
+import com.codecool.model.Mentor;
+
+import java.util.List;
 
 public class MentorSQL implements IMentorDao, IAbleToTeachDao {
+    @Override
+    public void addMentorToClass(Mentor mentor) {
+
+    }
+
+    @Override
+    public void getAllStudentsFromClass(Mentor mentor) {
+
+    }
+
+    @Override
+    public List<Mentor> getAllMentors() {
+        return null;
+    }
+
+    @Override
+    public Mentor getMentor(int id) {
+        return null;
+    }
+
+    @Override
+    public void updateMentor(Mentor mentor) {
+
+    }
+
+    @Override
+    public void addMentor(Mentor mentor) {
+
+    }
+
+    @Override
+    public void removeMentor(Mentor mentor) {
+
+    }
 }
