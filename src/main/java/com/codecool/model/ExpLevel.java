b4069c995bc3b2ab5d3f939f2d84fa90e1089b8a
Krzysiek <krzysiekczaja@gmail.com>
2019-07-09 13:09:28 +0200

    Add GroupClass implementation

--- src/main/java/com/codecool/model/ExpLevel.java
+++ src/main/java/com/codecool/model/ExpLevel.java
@@ -1,4 +1,37 @@
 package com.codecool.model;
 
 public class ExpLevel {
+    private int id;
+    private String className;
+    private int mentorId;
+
+    public ExpLevel(int id, String className, int mentorId) {
+        this.id = id;
+        this.className = className;
+        this.mentorId = mentorId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public int getMentorId() {
+        return mentorId;
+    }
+
+    public void setMentorId(int mentorId) {
+        this.mentorId = mentorId;
+    }
 }
