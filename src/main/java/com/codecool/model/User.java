58b37f6c6fb1a49f24bc38752eb45ec8752714b8
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-07-09 11:02:53 +0200

    Implement builder pattern for User class

--- src/main/java/com/codecool/model/User.java
+++ src/main/java/com/codecool/model/User.java
@@ -1,4 +1,104 @@
 package com.codecool.model;
 
-public abstract class User {
+public class User {
+    private int id;
+    private String type;
+    private UserCredentials userCredentials;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    private User() {}
+
+    public static class UserBuilder {
+        private User userToBuild;
+
+        public UserBuilder() {
+            this.userToBuild = new User();
+        }
+
+        public User build() {
+            User buildUser = this.userToBuild;
+            this.userToBuild = new User();
+            return buildUser;
+        }
+
+        public UserBuilder setId(int id) {
+            this.userToBuild.id = id;
+            return this;
+        }
+
+        public UserBuilder setType(String type) {
+            this.userToBuild.type = type;
+            return this;
+        }
+
+        public UserBuilder setUserCredentials(UserCredentials userCredentials) {
+            this.userToBuild.userCredentials = userCredentials;
+            return this;
+        }
+
+        public UserBuilder setFirstName(String firstName) {
+            this.userToBuild.firstName = firstName;
+            return this;
+        }
+
+        public UserBuilder setLastName(String lastName) {
+            this.userToBuild.lastName = lastName;
+            return this;
+        }
+
+        public UserBuilder setEmail(String email) {
+            this.userToBuild.email = email;
+            return this;
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public UserCredentials getUserCredentials() {
+        return userCredentials;
+    }
+
+    public void setUserCredentials(UserCredentials userCredentials) {
+        this.userCredentials = userCredentials;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
