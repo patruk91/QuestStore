d07180583b7ba3d286ea910bf131bab10687a37a
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-07-09 15:38:22 +0200

    Implement builder pattern for admin class

--- src/main/java/com/codecool/model/Admin.java
+++ src/main/java/com/codecool/model/Admin.java
@@ -1,4 +1,26 @@
 package com.codecool.model;
 
 public class Admin extends User {
+    protected Admin(UserBuilder<?> builder) {
+        super(builder);
+    }
+
+    public static class AdminBuilder extends User.UserBuilder<AdminBuilder> {
+        private Admin adminToBuild;
+
+        public AdminBuilder() {
+            this.adminToBuild = new Admin(this);
+        }
+
+        public Admin build() {
+            Admin buildUser = this.adminToBuild;
+            this.adminToBuild = new Admin(this);
+            return buildUser;
+        }
+
+        @Override
+        AdminBuilder getThis() {
+            return this;
+        }
+    }
 }
