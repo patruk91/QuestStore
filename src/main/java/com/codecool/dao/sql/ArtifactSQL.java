1472e479ce4636f9bc3ce114ea75001d73aa5f8c
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-07-11 13:41:11 +0200

    Add method to: insert new artifact data into database

--- src/main/java/com/codecool/dao/sql/ArtifactSQL.java
+++ src/main/java/com/codecool/dao/sql/ArtifactSQL.java
@@ -4,6 +4,9 @@ import com.codecool.dao.IArtifactDao;
 import com.codecool.model.Artifact;
 import com.codecool.model.Student;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class ArtifactSQL implements IArtifactDao {
@@ -15,7 +18,32 @@ public class ArtifactSQL implements IArtifactDao {
 
     @Override
     public void addArtifact(Artifact artifact) {
+        try {
+            Connection connection = connectionPool.getConnection();
+            addArtifactToDatabase(artifact, connection);
+            connectionPool.releaseConnection(connection);
+        } catch (SQLException e) {
+            System.err.println("SQLException: " + e.getMessage()
+                    + "\nSQLState: " + e.getSQLState()
+                    + "\nVendorError: " + e.getErrorCode());
+        }
+    }
 
+    private void addArtifactToDatabase(Artifact artifact, Connection connection) throws SQLException {
+        try (PreparedStatement stmtInsertArtifactData = connection.prepareStatement(
+                "INSERT INTO artifacts(id, name, description, price, image_link, category) VALUES(?, ?, ?, ?, ?, ?)")) {
+            insertArtifactData(stmtInsertArtifactData, artifact);
+        }
+    }
+
+    private void insertArtifactData(PreparedStatement stmtInsertArtifactData, Artifact artifact) throws SQLException {
+        stmtInsertArtifactData.setInt(1, artifact.getId());
+        stmtInsertArtifactData.setString(2, artifact.getName());
+        stmtInsertArtifactData.setString(3, artifact.getDescription());
+        stmtInsertArtifactData.setInt(4, artifact.getPrice());
+        stmtInsertArtifactData.setString(5, artifact.getImageLink());
+        stmtInsertArtifactData.setString(6, artifact.getCategory().toString());
+        stmtInsertArtifactData.executeUpdate();
     }
 
     @Override
