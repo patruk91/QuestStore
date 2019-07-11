8475cf10375f81046e9365bfcc16abf75eafb347
Krzysiek <krzysiekczaja@gmail.com>
2019-07-11 14:11:21 +0200

    Add method to add new class to database

--- src/main/java/com/codecool/dao/sql/ClassSQL.java
+++ src/main/java/com/codecool/dao/sql/ClassSQL.java
@@ -5,6 +5,9 @@ import com.codecool.model.ClassGroup;
 import com.codecool.model.Mentor;
 import com.codecool.model.Student;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class ClassSQL implements IClassDao {
@@ -16,7 +19,25 @@ public class ClassSQL implements IClassDao {
 
     @Override
     public void addClass(ClassGroup classGroup) {
+        String query = "INSERT INTO classes (class_name) VALUES (?)";
 
+        try {
+            Connection connection = connectionPool.getConnection();
+            insertClassData(query, connection, classGroup);
+            connectionPool.releaseConnection(connection);
+        } catch (SQLException e) {
+            System.err.println("SQLException: " + e.getMessage()
+                    + "\nSQLState: " + e.getSQLState()
+                    + "\nVendorError: " + e.getErrorCode());
+        }
+    }
+
+    private void insertClassData(String query, Connection connection, ClassGroup classGroup) throws SQLException {
+        try (PreparedStatement stmt = connection.prepareStatement(query)) {
+            stmt.setString(1, classGroup.getClassName());
+
+            stmt.executeUpdate();
+        }
     }
 
     @Override
