4ac349799cb58126b6cca0055d150b18ae8145dc
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-07-11 15:47:50 +0200

    Add method to: insert new experience level

--- src/main/java/com/codecool/dao/sql/ExpLevelSQL.java
+++ src/main/java/com/codecool/dao/sql/ExpLevelSQL.java
@@ -2,7 +2,11 @@ package com.codecool.dao.sql;
 
 import com.codecool.dao.IExpLevelDao;
 import com.codecool.model.ExpLevel;
+import com.codecool.model.Student;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class ExpLevelSQL implements IExpLevelDao {
@@ -14,7 +18,28 @@ public class ExpLevelSQL implements IExpLevelDao {
 
     @Override
     public void addExpLevel(ExpLevel expLevel) {
+        try {
+            Connection connection = connectionPool.getConnection();
+            addExpLevelToDatabase(expLevel, connection);
+            connectionPool.releaseConnection(connection);
+        } catch (SQLException e) {
+            System.err.println("SQLException: " + e.getMessage()
+                    + "\nSQLState: " + e.getSQLState()
+                    + "\nVendorError: " + e.getErrorCode());
+        }
+    }
 
+    private void addExpLevelToDatabase(ExpLevel expLevel, Connection connection) throws SQLException {
+        try (PreparedStatement stmtInsertExpLevelData = connection.prepareStatement(
+                "INSERT INTO experience_levels(name, experience_amount) VALUES(?, ?)")) {
+            insertExpLevel(stmtInsertExpLevelData, expLevel);
+        }
+    }
+
+    private void insertExpLevel(PreparedStatement stmtInsertExpLevelData, ExpLevel expLevel) throws SQLException {
+        stmtInsertExpLevelData.setString(1, expLevel.getName());
+        stmtInsertExpLevelData.setString(2, expLevel.getName());
+        stmtInsertExpLevelData.executeUpdate();
     }
 
     @Override
