912642bb726a7e5ca47815ad2a5100549278ac6b
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-07-11 16:04:58 +0200

    Add methods to: update experience levels and remove last level

--- src/main/java/com/codecool/dao/sql/ExpLevelSQL.java
+++ src/main/java/com/codecool/dao/sql/ExpLevelSQL.java
@@ -44,12 +44,49 @@ public class ExpLevelSQL implements IExpLevelDao {
 
     @Override
     public void updateExpLevel(ExpLevel expLevel) {
+        try {
+            Connection connection = connectionPool.getConnection();
+            updateExpLevelData(connection, expLevel);
+            connectionPool.releaseConnection(connection);
+        } catch (SQLException e) {
+            System.err.println("SQLException: " + e.getMessage()
+                    + "\nSQLState: " + e.getSQLState()
+                    + "\nVendorError: " + e.getErrorCode());
+        }
+    }
 
+    private void updateExpLevelData(Connection connection, ExpLevel expLevel) throws SQLException {
+        try (PreparedStatement stmtUpdateExpLevelData = connection.prepareStatement(
+                "UPDATE experience_levels SET experience_amount = ? WHERE name = ?")) {
+            updateExpLevelInDatabase(stmtUpdateExpLevelData, expLevel);
+        }
+    }
+
+    private void updateExpLevelInDatabase(PreparedStatement stmtUpdateExpLevelData, ExpLevel expLevel) throws SQLException {
+        stmtUpdateExpLevelData.setInt(1, expLevel.getExpAmount());
+        stmtUpdateExpLevelData.setString(2, expLevel.getName());
     }
 
     @Override
     public void removeLastExpLevel() {
+        try {
+            Connection connection = connectionPool.getConnection();
+            removeLevelFromDatabase(connection);
+            connectionPool.releaseConnection(connection);
+        } catch (SQLException e) {
+            System.err.println("SQLException: " + e.getMessage()
+                    + "\nSQLState: " + e.getSQLState()
+                    + "\nVendorError: " + e.getErrorCode());
+        }
+    }
 
+    private void removeLevelFromDatabase(Connection connection) throws SQLException {
+        try(PreparedStatement stmt = connection.prepareStatement(
+                "DELETE FROM users  WHERE id = (SELECT * FROM experience_levels\n" +
+                        "ORDER BY name DESC\n" +
+                        "LIMIT 1)")) {
+            stmt.executeUpdate();
+        }
     }
 
     @Override
