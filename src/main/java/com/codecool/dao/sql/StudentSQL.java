685f8771676702146b3657faa126165b64c41612
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-07-11 11:08:02 +0200

    Implement method to: get single student by id  from database

--- src/main/java/com/codecool/dao/sql/StudentSQL.java
+++ src/main/java/com/codecool/dao/sql/StudentSQL.java
@@ -1,7 +1,6 @@
 package com.codecool.dao.sql;
 
 import com.codecool.dao.IStudentDao;
-import com.codecool.model.Mentor;
 import com.codecool.model.Student;
 import com.codecool.model.UserCredentials;
 
@@ -39,7 +38,7 @@ public class StudentSQL implements IStudentDao{
         List<Student> students = new ArrayList<>();
         try {
             Connection connection = connectionPool.getConnection();
-            prepareStmtForGetAllStudentsQuery(students, connection);
+            addStudents(students, connection);
             connectionPool.releaseConnection(connection);
         } catch (SQLException e) {
             System.err.println("SQLException: " + e.getMessage()
@@ -49,7 +48,7 @@ public class StudentSQL implements IStudentDao{
         return students;
     }
 
-    private void prepareStmtForGetAllStudentsQuery(List<Student> students, Connection connection) throws SQLException {
+    private void addStudents(List<Student> students, Connection connection) throws SQLException {
         try(PreparedStatement stmt = connection.prepareStatement(
                 "SELECT users.id, users.type, users.credential_id, users.first_name, users.last_name,\n" +
                         "users.email, cred.login, cred.password, profile.coins, profile.experience,\n" +
@@ -64,36 +63,73 @@ public class StudentSQL implements IStudentDao{
     private void addStudentToList(PreparedStatement stmt, List<Student> students) throws SQLException {
         try (ResultSet resultSet = stmt.executeQuery()) {
             while (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                String type = resultSet.getString("type");
-                String login = resultSet.getString("login");
-                String password = resultSet.getString("password");
-                UserCredentials userCredentials = new UserCredentials(login, password);
-                String firstName = resultSet.getString("first_name");
-                String lastName = resultSet.getString("last_name");
-                String email = resultSet.getString("email");
-                int coins = resultSet.getInt("coins");
-                int experience = resultSet.getInt("experience");
-                int classId = resultSet.getInt("experience");
-
-                Student.StudentBuilder studentBuilder = new Student.StudentBuilder();
-                Student student = studentBuilder
-                        .setId(id)
-                        .setType(type)
-                        .setUserCredentials(userCredentials)
-                        .setFirstName(firstName)
-                        .setLastName(lastName)
-                        .setEmail(email)
-                        .setClassId(coins)
-                        .setCoins(experience)
-                        .setExperience(classId).build();
+                Student student = buildSingleStudent(resultSet);
                 students.add(student);
             }
         }
     }
 
+    private Student buildSingleStudent(ResultSet resultSet) throws SQLException {
+        int id = resultSet.getInt("id");
+        String type = resultSet.getString("type");
+        String login = resultSet.getString("login");
+        String password = resultSet.getString("password");
+        UserCredentials userCredentials = new UserCredentials(login, password);
+        String firstName = resultSet.getString("first_name");
+        String lastName = resultSet.getString("last_name");
+        String email = resultSet.getString("email");
+        int coins = resultSet.getInt("coins");
+        int experience = resultSet.getInt("experience");
+        int classId = resultSet.getInt("experience");
+
+        Student.StudentBuilder studentBuilder = new Student.StudentBuilder();
+        return studentBuilder
+                .setId(id)
+                .setType(type)
+                .setUserCredentials(userCredentials)
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setEmail(email)
+                .setClassId(coins)
+                .setCoins(experience)
+                .setExperience(classId).build();
+    }
+
     @Override
     public Student getStudent(int id) {
-        return null;
+        Student student = null;
+        try {
+            Connection connection = connectionPool.getConnection();
+            student = getSingleStudent(connection, id);
+            connectionPool.releaseConnection(connection);
+        } catch (SQLException e) {
+            System.err.println("SQLException: " + e.getMessage()
+                    + "\nSQLState: " + e.getSQLState()
+                    + "\nVendorError: " + e.getErrorCode());
+        }
+        return student;
+    }
+
+    private Student getSingleStudent(Connection connection, int id) throws SQLException {
+        try(PreparedStatement stmt = connection.prepareStatement(
+                "SELECT users.id, users.type, users.credential_id, users.first_name, users.last_name,\n" +
+                        "users.email, cred.login, cred.password, profile.coins, profile.experience,\n" +
+                        "profile.class_id FROM users\n" +
+                        "JOIN user_credentials AS cred ON users.id = cred.user_id\n" +
+                        "JOIN students_profiles AS profile on users.id = profile.student_id\n" +
+                        "WHERE id = ?")) {
+            stmt.setInt(1, id);
+            return getSingleStudentData(stmt);
+        }
+    }
+
+    private Student getSingleStudentData(PreparedStatement stmt) throws SQLException {
+        Student student = null;
+        try (ResultSet resultSet = stmt.executeQuery()) {
+            while (resultSet.next()) {
+                student = buildSingleStudent(resultSet);
+            }
+            return student;
+        }
     }
 }
