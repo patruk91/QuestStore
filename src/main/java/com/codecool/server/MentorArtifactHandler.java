bbbae69d6a624f2d39aabccbe98ceff66ba83f48
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-07-25 14:04:19 +0200

    Add context to server: handler for quest, artifacts, mentors main page

--- src/main/java/com/codecool/server/MentorArtifactHandler.java
+++ src/main/java/com/codecool/server/MentorArtifactHandler.java
@@ -1,20 +1,20 @@
 package com.codecool.server;
 
+import com.codecool.dao.IArtifactDao;
 import com.codecool.dao.ISessionDao;
-import com.codecool.dao.IStudentDao;
 import com.codecool.server.helper.CommonHelper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
 
-public class MentorHandler implements HttpHandler {
-    private IStudentDao studentDao;
+public class MentorArtifactHandler implements HttpHandler {
+    private IArtifactDao artifactDao;
     private ISessionDao sessionDao;
     private CommonHelper commonHelper;
 
-    public MentorHandler(IStudentDao studentDao, ISessionDao sessionDao, CommonHelper commonHelper) {
-        this.studentDao = studentDao;
+    public MentorArtifactHandler(IArtifactDao artifactDao, ISessionDao sessionDao, CommonHelper commonHelper) {
+        this.artifactDao = artifactDao;
         this.sessionDao = sessionDao;
         this.commonHelper = commonHelper;
     }
--- src/main/java/com/codecool/server/MentorQuestHandler.java
+++ src/main/java/com/codecool/server/MentorQuestHandler.java
@@ -1,20 +1,20 @@
 package com.codecool.server;
 
+import com.codecool.dao.IQuestDao;
 import com.codecool.dao.ISessionDao;
-import com.codecool.dao.IStudentDao;
 import com.codecool.server.helper.CommonHelper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
 
-public class MentorHandler implements HttpHandler {
-    private IStudentDao studentDao;
+public class MentorQuestHandler implements HttpHandler {
+    private IQuestDao questDao;
     private ISessionDao sessionDao;
     private CommonHelper commonHelper;
 
-    public MentorHandler(IStudentDao studentDao, ISessionDao sessionDao, CommonHelper commonHelper) {
-        this.studentDao = studentDao;
+    public MentorQuestHandler(IQuestDao questDao, ISessionDao sessionDao, CommonHelper commonHelper) {
+        this.questDao = questDao;
         this.sessionDao = sessionDao;
         this.commonHelper = commonHelper;
     }
