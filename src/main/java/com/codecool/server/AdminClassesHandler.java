eb78be85637fec3f0afc77b25dda4ae2318976ad
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-08-20 14:43:30 +0200

    Implement override handle method

--- src/main/java/com/codecool/server/AdminClassesHandler.java
+++ src/main/java/com/codecool/server/AdminClassesHandler.java
@@ -7,6 +7,9 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
+import java.net.HttpCookie;
+import java.net.URI;
+import java.util.Map;
 
 public class AdminClassesHandler implements HttpHandler {
     private IClassDao classDao;
@@ -20,7 +23,71 @@ public class AdminClassesHandler implements HttpHandler {
     }
 
     @Override
-    public void handle(HttpExchange exchange) throws IOException {
+    public void handle(HttpExchange httpExchange) throws IOException {
+        String response = "";
+        String method = httpExchange.getRequestMethod();
+        String cookieStr = httpExchange.getRequestHeaders().getFirst("Cookie");
+        HttpCookie cookie;
+        if (cookieStr != null) {
+            cookie = HttpCookie.parse(cookieStr).get(0);
+            if (sessionDao.isCurrentSession(cookie.getValue())) {
+                int userId = sessionDao.getUserIdBySessionId(cookie.getValue());
+                response = handleRequest(httpExchange, userId, method);
+            } else {
+                commonHelper.redirectToUserPage(httpExchange, "/");
+            }
+        } else {
+            commonHelper.redirectToUserPage(httpExchange, "/");
+        }
+    commonHelper.sendResponse(httpExchange, response);
+    }
 
+    private String handleRequest(HttpExchange httpExchange, int userId, String method) throws IOException {
+        String response = "";
+        URI uri = httpExchange.getRequestURI();
+        Map<String, String> parsedUri = commonHelper.parseURI(uri.getPath());
+        String action = "index";
+        int classesId = 0;
+        if(!parsedUri.isEmpty()) {
+            action = parsedUri.keySet().iterator().next();
+            classesId = Integer.parseInt(parsedUri.get(action));
+        }
+        switch (action) {
+            case "index":
+                response = index(userId);
+                httpExchange.sendResponseHeaders(200, response.getBytes().length);
+                break;
+            case "add":
+                response = add(method, httpExchange);
+                break;
+            case "view":
+                response = view(classesId, httpExchange);
+                break;
+            case "edit":
+                response = edit(classesId, method, httpExchange);
+                break;
+            case "delete":
+                delete(classesId, httpExchange);
+                break;
+            default:
+                response = index(userId);
+                break;
+        }
+        return response;
+    }
+
+    private void delete(int classesId, HttpExchange httpExchange) {
+    }
+
+    private String edit(int classesId, String method, HttpExchange httpExchange) {
+    }
+
+    private String view(int classesId, HttpExchange httpExchange) {
+    }
+
+    private String add(String method, HttpExchange httpExchange) {
+    }
+
+    private String index(int userId) {
     }
 }
