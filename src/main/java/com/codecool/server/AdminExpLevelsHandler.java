242f443a970de31a6a8a8bf02f454999cb547045
CzajaKrzysztof <krzysiekczaja@gmail.com>
2019-08-20 14:44:18 +0200

    Add base methods to handle requests in operations methods

--- src/main/java/com/codecool/server/AdminExpLevelsHandler.java
+++ src/main/java/com/codecool/server/AdminExpLevelsHandler.java
@@ -7,6 +7,9 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
+import java.net.HttpCookie;
+import java.net.URI;
+import java.util.Map;
 
 public class AdminExpLevelsHandler implements HttpHandler {
     private IExpLevelDao expLevelDao;
@@ -20,7 +23,58 @@ public class AdminExpLevelsHandler implements HttpHandler {
     }
 
     @Override
-    public void handle(HttpExchange exchange) throws IOException {
+    public void handle(HttpExchange httpExchange) throws IOException {
+        String response = "";
+        String method = httpExchange.getRequestMethod();
+        String cookieStr = httpExchange.getRequestHeaders().getFirst("Cookie");
+        HttpCookie cookie;
+        if (method.equals("GET") || method.equals("POST")) {
+            if (cookieStr != null) {
+                cookie = HttpCookie.parse(cookieStr).get(0);
+                if (sessionDao.isCurrentSession(cookie.getValue())) {
+                    int userId = sessionDao.getUserIdBySessionId(cookie.getValue());
+                    response = handleRequest(httpExchange, userId, method);
+                } else {
+                    commonHelper.redirectToUserPage(httpExchange, "/");
+                }
+            } else {
+                commonHelper.redirectToUserPage(httpExchange, "/");
+            }
+        }
+        commonHelper.sendResponse(httpExchange, response);
+    }
 
+    private String handleRequest(HttpExchange httpExchange, int userId, String method) throws IOException {
+        String response = "";
+        URI uri = httpExchange.getRequestURI();
+        Map<String, String> parsedUri = commonHelper.parseURI(uri.getPath());
+        String action = "index";
+        String expLevelName = "";
+        if(!parsedUri.isEmpty()) {
+            action = parsedUri.keySet().iterator().next();
+            expLevelName = (parsedUri.get(action);
+        }
+        switch (action) {
+            case "index":
+                response = index(userId);
+                httpExchange.sendResponseHeaders(200, response.getBytes().length);
+                break;
+            case "add":
+                response = add(method, httpExchange);
+                break;
+            case "view":
+                response = view(expLevelName, httpExchange);
+                break;
+            case "edit":
+                response = edit(expLevelName, method, httpExchange);
+                break;
+            case "delete":
+                delete(expLevelName, httpExchange);
+                break;
+            default:
+                response = index(userId);
+                break;
+        }
+        return response;
     }
 }
