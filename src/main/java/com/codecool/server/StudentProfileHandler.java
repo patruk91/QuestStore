eed8cb297494b9c82850b75aca407adde7dd4042
CzajaKrzysztof <krzysiekczaja@gmail.com>
2019-08-21 17:02:57 +0200

    Add implementation of handler for user profile

--- src/main/java/com/codecool/server/StudentProfileHandler.java
+++ src/main/java/com/codecool/server/StudentProfileHandler.java
@@ -1,25 +1,41 @@
 package com.codecool.server;
 
-import com.codecool.dao.IExpLevelDao;
-import com.codecool.dao.ISessionDao;
+import com.codecool.dao.*;
+import com.codecool.model.Artifact;
+import com.codecool.model.ExpLevel;
+import com.codecool.model.Student;
 import com.codecool.server.helper.CommonHelper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import org.jtwig.JtwigModel;
+import org.jtwig.JtwigTemplate;
 
 import java.io.IOException;
 import java.net.HttpCookie;
 import java.net.URI;
+import java.util.List;
 import java.util.Map;
 
 public class StudentProfileHandler implements HttpHandler {
     private IExpLevelDao expLevelDao;
     private ISessionDao sessionDao;
     private CommonHelper commonHelper;
+    private IMentorDao mentorDao;
+    private IStudentDao studentDao;
+    private IClassDao classDao;
+    private IArtifactDao artifactDao;
 
-    public StudentProfileHandler(IExpLevelDao expLevelDao, ISessionDao sessionDao, CommonHelper commonHelper) {
+    public StudentProfileHandler(IExpLevelDao expLevelDao, ISessionDao sessionDao,
+                                 CommonHelper commonHelper, IMentorDao mentorDao,
+                                 IStudentDao studentDao, IClassDao classDao,
+                                 IArtifactDao artifactDao) {
         this.expLevelDao = expLevelDao;
         this.sessionDao = sessionDao;
         this.commonHelper = commonHelper;
+        this.mentorDao = mentorDao;
+        this.studentDao = studentDao;
+        this.classDao = classDao;
+        this.artifactDao = artifactDao;
     }
 
     @Override
@@ -45,24 +61,28 @@ public class StudentProfileHandler implements HttpHandler {
     }
 
     private String handleRequest(HttpExchange httpExchange, int userId, String method) throws IOException {
-        String response = "";
-        URI uri = httpExchange.getRequestURI();
-        Map<String, String> parsedUri = commonHelper.parseURI(uri.getPath());
-        String action = "index";
-        int mentorId = 0;
-        if(!parsedUri.isEmpty()) {
-            action = parsedUri.keySet().iterator().next();
-            mentorId = Integer.parseInt(parsedUri.get(action));
-        }
-        switch (action) {
-            case "index":
-                response = index(userId);
-                httpExchange.sendResponseHeaders(200, response.getBytes().length);
-                break;
-            default:
-                response = index(userId);
-                break;
-        }
+        String fullName = String.format("%s %s", mentorDao.getMentor(userId).getFirstName(),
+                mentorDao.getMentor(userId).getLastName());
+        JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/profile.twig");
+        JtwigModel model = JtwigModel.newModel();
+        Student student = studentDao.getStudent(userId);
+        String className = classDao.getClassName(student);
+        String expLevelName = expLevelDao.getExpLevelName(student.getExperience());
+        ExpLevel expLevel = expLevelDao.getExpLevel(expLevelName);
+        int expAmountHave = student.getExperience() - expLevel.getExpAmountAtStart();
+        int expPointsNeed = expLevel.getExpAmountAtEnd() - expAmountHave;
+        int expPercentageAnount = expAmountHave / expLevel.getExpAmountAtEnd() * 100;
+        List<Artifact> artifacts = artifactDao.getAllArtifactsByStudentId(userId, true);
+
+        model.with("fullName", fullName);
+        model.with("artifacts", artifacts);
+        model.with("expPercentageAnount", expPercentageAnount);
+        model.with("expPointAmount", student.getExperience());
+        model.with("expPointsNeed", expPointsNeed);
+        model.with("expLevelName", expLevelName);
+        model.with("className", className);
+        String response = template.render(model);
+        httpExchange.sendResponseHeaders(200, response.getBytes().length);
         return response;
     }
 }
