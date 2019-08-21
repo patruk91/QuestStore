c4d9171999ac8f76651b99a967b3a19007b3400c
CzajaKrzysztof <krzysiekczaja@gmail.com>
2019-08-21 12:51:23 +0200

    Add base methods to handle requests by appropriate methods

--- src/main/java/com/codecool/server/MentorArtifactHandler.java
+++ src/main/java/com/codecool/server/MentorArtifactHandler.java
@@ -8,6 +8,9 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
+import java.net.HttpCookie;
+import java.net.URI;
+import java.util.Map;
 
 public class MentorArtifactHandler implements HttpHandler {
     private IArtifactDao artifactDao;
@@ -23,7 +26,57 @@ public class MentorArtifactHandler implements HttpHandler {
     }
 
     @Override
-    public void handle(HttpExchange exchange) throws IOException {
+    public void handle(HttpExchange httpExchange) throws IOException {
+        String response = "";
+        String method = httpExchange.getRequestMethod();
+        String cookieStr = httpExchange.getRequestHeaders().getFirst("Cookie");
+        HttpCookie cookie;
+        if (method.equals("GET") || method.equals("POST")) {
+            if (cookieStr != null) {
+                cookie = HttpCookie.parse(cookieStr).get(0);
+                if (sessionDao.isCurrentSession(cookie.getValue())) {
+                    int userId = sessionDao.getUserIdBySessionId(cookie.getValue());
+                    response = handleRequest(httpExchange, userId, method);
+                } else {
+                    commonHelper.redirectToUserPage(httpExchange, "/");
+                }
+            } else {
+                commonHelper.redirectToUserPage(httpExchange, "/");
+            }
+        }
+        commonHelper.sendResponse(httpExchange, response);
+    }
 
+    private String handleRequest(HttpExchange httpExchange, int userId, String method) throws IOException {
+        String response = "";
+        URI uri = httpExchange.getRequestURI();
+        Map<String, String> parsedUri = commonHelper.parseURI(uri.getPath());
+        String action = "index";
+        int artifactId = 0;
+        if(!parsedUri.isEmpty()) {
+            action = parsedUri.keySet().iterator().next();
+            artifactId = Integer.parseInt(parsedUri.get(action));
+        }
+        switch (action) {
+            case "index":
+//                response = index(userId, httpExchange);
+                break;
+            case "add":
+//                response = add(method, httpExchange);
+                break;
+            case "view":
+//                response = view(artifactId, httpExchange);
+                break;
+            case "edit":
+//                response = edit(artifactId, method, httpExchange);
+                break;
+            case "delete":
+//                delete(artifactId, httpExchange);
+                break;
+            default:
+//                response = index(userId, httpExchange);
+                break;
+        }
+        return response;
     }
 }
