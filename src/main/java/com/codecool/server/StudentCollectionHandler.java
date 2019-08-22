ac6e300095c67a8b093d2f2ddf39cdb4bdde186b
CzajaKrzysztof <krzysiekczaja@gmail.com>
2019-08-22 10:01:49 +0200

    Add basic methods to redirect request to appropriate operation method

--- src/main/java/com/codecool/server/StudentCollectionHandler.java
+++ src/main/java/com/codecool/server/StudentCollectionHandler.java
@@ -7,6 +7,9 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
+import java.net.HttpCookie;
+import java.net.URI;
+import java.util.Map;
 
 public class StudentCollectionHandler implements HttpHandler {
     private ICollectionGroupDao collectionGroupDao;
@@ -20,7 +23,47 @@ public class StudentCollectionHandler implements HttpHandler {
     }
 
     @Override
-    public void handle(HttpExchange exchange) throws IOException {
+    public void handle(HttpExchange httpExchange) throws IOException {
+        String response = "";
+        String method = httpExchange.getRequestMethod();
+        String cookieStr = httpExchange.getRequestHeaders().getFirst("Cookie");
+        HttpCookie cookie;
+        if (cookieStr != null) {
+            cookie = HttpCookie.parse(cookieStr).get(0);
+            if (sessionDao.isCurrentSession(cookie.getValue())) {
+                int userId = sessionDao.getUserIdBySessionId(cookie.getValue());
+                response = handleRequest(httpExchange, userId, method);
+            } else {
+                commonHelper.redirectToUserPage(httpExchange, "/");
+            }
+        } else {
+            commonHelper.redirectToUserPage(httpExchange, "/");
+        }
+        commonHelper.sendResponse(httpExchange, response);
+    }
 
+    private String handleRequest(HttpExchange httpExchange, int userId, String method) throws IOException {
+        String response = "";
+        URI uri = httpExchange.getRequestURI();
+        Map<String, String> parsedUri = commonHelper.parseURI(uri.getPath());
+        String action = "index";
+        int collectionId = 0;
+        if(!parsedUri.isEmpty()) {
+            action = parsedUri.keySet().iterator().next();
+            collectionId = Integer.parseInt(parsedUri.get(action));
+        }
+        switch (action) {
+            case "index":
+                response = index(userId);
+                httpExchange.sendResponseHeaders(200, response.getBytes().length);
+                break;
+            case "donate":
+                response = donate(collectionId, method, httpExchange);
+                break;
+            default:
+                response = index(userId);
+                break;
+        }
+        return response;
     }
 }
